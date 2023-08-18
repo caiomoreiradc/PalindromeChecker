afd043f2a4062499bd60e78b0d2cdb922c722b02
caiomoreiradc <caiomoreiracarvalhoo@outlook.com>
2023-08-18 17:10:00 -0300

    Add project files.

---
+++ PalindromeChecker/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace PalindromeChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool palindrome = false;
+
+            Console.Write("Enter the word: ");
+            string word = Console.ReadLine().ToLower();
+
+            string wordReverse = new string(word.Reverse().ToArray());
+
+
+
+            palindrome = CheckPalindrome(word, wordReverse);
+
+            ShowResults(word, palindrome);
+
+            Console.ReadKey();
+        }
+        private static bool CheckPalindrome(string word, string wordReverse)
+        {
+            bool palindrome;
+            if (word == wordReverse)
+            {
+                palindrome = true;
+            }
+            else
+            {
+                palindrome = false;
+            }
+
+            return palindrome;
+        }
+        private static void ShowResults(string word, bool palindrome)
+        {
+            if (palindrome)
+                Console.WriteLine($"{word} is a palindrome");
+            else
+                Console.WriteLine($"{word} is not a palindrome");
+        }
+    }
+}
